24d887076106a4d6e26b6fabf3bc63725e4cfe7b
nemish <nsi@taglineinfotech.com>
2025-09-19 17:48:15 +0530

    Initial project setup

---
+++ chatbot_project/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for chatbot_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot_project.settings")
+
+application = get_wsgi_application()
